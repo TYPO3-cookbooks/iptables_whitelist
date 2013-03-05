308bb00e727fa3f0039a70783f37777547e65e2f
Steffen Gebert <steffen.gebert@typo3.org>
2013-03-05 17:56:56 +0100

    Initial version

---
+++ recipes/default.rb
@@ -0,0 +1,31 @@
+include_recipe "simple_iptables"
+
+dport = 2003
+role_search = "*"
+
+nodes = {}
+
+# for testing with e.g. vagrant (then uncomment the next paragraph!)
+# nodes = { :your_machine => "192.168.156.1", :other_machine => "192.168.156.2" }
+
+search(:node, "role:#{role_search}").each do |n|
+  nodes << {n[:fqdn] => n[:ipaddress]} unless nil? n[:ipaddress]
+end
+
+Chef::Log.info "Whitelisting the following IPs for port #{dport}: " +  nodes.inspect
+
+# ACCEPT all known hosts
+nodes.values.sort.each do |ip|
+  simple_iptables_rule "graphite" do
+    direction "INPUT"
+    rule "--proto tcp --dport #{dport} --src #{ip}"
+    jump "ACCEPT"
+  end
+end
+
+# DROP everything else
+simple_iptables_rule "graphite" do
+  direction "INPUT"
+  rule "--proto tcp --dport #{dport}"
+  jump "DROP"
+end
